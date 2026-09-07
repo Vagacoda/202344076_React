4e030900220f6bb52c09b8a9af4b1681a84df9a0
Vagacoda <gtavictsaven147@gmail.com>
2026-09-07 14:51:28 +0900

    2026/09/07 - 14:51 test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-import Component from './components01/practice/StylePage'
+import Component from './components01/practice/MainPage'
 
 const App = ()=> {
     return (
         <div>
+            <Component/>
             <Component/>
         </div>
     );
--- src/components01/practice/StylePage.jsx
+++ src/components01/practice/StylePage.jsx
@@ -4,11 +4,16 @@
 import '../Style01.css'
 
 const StylePage = () => {
+    const style = {
+        fontSize : '30px',
+        FontFamily: '궁서체',
+        color : 'green',
+    }
     return (
         <div className='box'>
-            <h1 style={{fontSize:'50px'}}>안녕 리액트!</h1>
-            <h2>저는 홍길동입니다.</h2>
-            <h3>만나서 반갑습니다.</h3>
+            <h1 style={{fontSize:'50px', color : 'red'}}>안녕 리액트!</h1>
+            <h2 style={style}>저는 홍길동입니다.</h2>
+            <h3 style={style}>만나서 반갑습니다.</h3>
         </div>
     )
 }
